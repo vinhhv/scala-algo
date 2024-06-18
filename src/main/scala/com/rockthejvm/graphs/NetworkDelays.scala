8d2e32f3be338b5e89f6ad40ed44f58c4ef42a61
Vinh Vu <vinhhv@uci.edu>
2024-06-18 13:51:23 -0700

    (hard) network delay leetcode

--- src/main/scala/com/rockthejvm/graphs/NetworkDelays.scala
+++ src/main/scala/com/rockthejvm/graphs/NetworkDelays.scala
@@ -26,7 +26,21 @@ object NetworkDelays {
    * 4. We keep track of max total distance from heap and return that once we visit all nodes
    */
 
-  def computerNetworkDelay(n: Int, times: List[(Int, Int, Int)]): Int = {
+  def networkDelayTime(times: Array[Array[Int]], n: Int, k: Int): Int = {
+    val newTimes: List[(Int, Int, Int)] = times.toList
+      .map(_.toList)
+      .map { time =>
+        time match {
+          case a :: b :: t :: tail => (a, b, t)
+          case _                   => throw new IllegalArgumentException
+        }
+      }
+      .toList
+
+    computerNetworkDelay(k, newTimes, n)
+  }
+
+  def computerNetworkDelay(n: Int, times: List[(Int, Int, Int)], k: Int = 0): Int = {
     @tailrec
     def recurse(
         pq: PriorityQueue[(Int, Int)],
@@ -34,7 +48,8 @@ object NetworkDelays {
         visited: Set[Int],
         maxDistance: Int
     ): Int = {
-      if (pq.isEmpty) maxDistance
+      if (pq.isEmpty && visited.size == k) maxDistance
+      else if (pq.isEmpty) -1
       else if (visited.contains(pq.head._1)) recurse(pq.drop(1), graph, visited, maxDistance)
       else {
         val (node, totalDistanceSoFar) = pq.dequeue()
@@ -55,11 +70,16 @@ object NetworkDelays {
 
     val orderingByTime: Ordering[(Int, Int)] = Ordering.by(-_._2)
     val pq                                   = PriorityQueue(graph.getOrElse(n, List.empty)*)(orderingByTime)
-    recurse(pq, graph, Set.empty, 0)
+    recurse(pq, graph, Set(n), 0)
   }
 
   def main(args: Array[String]): Unit = {
-    val times = List((1, 2, 3), (2, 3, 4), (3, 4, 2), (1, 3, 10), (1, 4, 10))
-    println(computerNetworkDelay(1, times))
+    val times       = List((1, 2, 3), (2, 3, 4), (3, 4, 2), (1, 3, 10), (1, 4, 10)) // 9
+    val brokenTimes = List((1, 2, 3), (2, 3, 4), (1, 3, 10))                        // -1
+    println(computerNetworkDelay(1, times, 4))
+    println(computerNetworkDelay(1, brokenTimes, 4))
+
+    val lc1 = Array(Array(2, 1, 1), Array(2, 3, 1), Array(3, 4, 1))
+    val lc2 = Array
   }
 }

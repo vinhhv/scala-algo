8f06601367a31c4abbeeaf624f02d656527f0457
Vinh Vu <vinhhv@uci.edu>
2024-06-17 20:14:50 -0700

    (medium) find paths

--- src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
+++ src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
@@ -1,5 +1,7 @@
 package com.rockthejvm.graphs
 
+import scala.collection.immutable.Queue
+
 object GraphProblems extends App {
 
   type Graph[T] = Map[T, Set[T]]
@@ -9,7 +11,9 @@ object GraphProblems extends App {
     "Bob"     -> Set(),
     "Charlie" -> Set("David"),
     "David"   -> Set("Bob", "Mary"),
-    "Mary"    -> Set("Bob", "Charlie")
+    "Mary"    -> Set("Bob", "Charlie"),
+    "Vinh"    -> Set("Sam"),
+    "Sam"     -> Set("Vinh")
   )
 
   // number of nodes this node `node` is associated (adjacent) to
@@ -19,6 +23,28 @@ object GraphProblems extends App {
   def inDegree[T](graph: Graph[T], node: T): Int =
     graph.values.count(_.contains(node))
 
+  def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
+    def recurse(queue: Queue[T], visited: Set[T]): Boolean = {
+      if (queue.isEmpty) false
+      else if (queue.head == end) true
+      else {
+        val (key, newQueue) = queue.dequeue
+        if (graph.contains(key)) {
+          val nodes         = graph(key)
+          val filteredNodes = nodes.filter(!visited.contains(_))
+          val updatedQueue  = newQueue.enqueueAll(filteredNodes)
+          recurse(updatedQueue, visited ++ filteredNodes)
+        } else recurse(newQueue, visited + key)
+      }
+    }
+
+    recurse(Queue(start), Set.empty)
+  }
+
   println(outDegree(socialNetwork, "Alice")) // 3
   println(inDegree(socialNetwork, "David"))  // 2
+
+  println(isPath(socialNetwork, "Alice", "Mary")) // true
+  println(isPath(socialNetwork, "Mary", "Alice")) // false
+  println(isPath(socialNetwork, "Vinh", "Alice")) // false
 }

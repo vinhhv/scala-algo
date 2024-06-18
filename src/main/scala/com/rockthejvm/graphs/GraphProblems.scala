2a51fa5748101e5776cb0e15bca835c06cfb7712
Vinh Vu <vinhhv@uci.edu>
2024-06-17 21:08:47 -0700

    (medium) find path 2

--- src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
+++ src/main/scala/com/rockthejvm/graphs/GraphProblems.scala
@@ -1,6 +1,7 @@
 package com.rockthejvm.graphs
 
 import scala.collection.immutable.Queue
+import scala.annotation.tailrec
 
 object GraphProblems extends App {
 
@@ -10,10 +11,10 @@ object GraphProblems extends App {
     "Alice"   -> Set("Bob", "Charlie", "David"),
     "Bob"     -> Set(),
     "Charlie" -> Set("David"),
-    "David"   -> Set("Bob", "Mary"),
+    "David"   -> Set("Bob", "Vinh", "Mary"),
     "Mary"    -> Set("Bob", "Charlie"),
     "Vinh"    -> Set("Sam"),
-    "Sam"     -> Set("Vinh")
+    "Sam"     -> Set("Bob", "Vinh", "Mary", "Alice")
   )
 
   // number of nodes this node `node` is associated (adjacent) to
@@ -24,6 +25,7 @@ object GraphProblems extends App {
     graph.values.count(_.contains(node))
 
   def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
+    @tailrec
     def recurse(queue: Queue[T], visited: Set[T]): Boolean = {
       if (queue.isEmpty) false
       else if (queue.head == end) true
@@ -41,10 +43,49 @@ object GraphProblems extends App {
     recurse(Queue(start), Set.empty)
   }
 
+  def findPath[T](graph: Graph[T], start: T, end: T): List[T] = {
+    // @tailrec
+    // def recurse(queue: List[T], visited: Set[T], path: List[T]): List[T] = {
+    //   if (queue.isEmpty) List.empty
+    //   else if (queue.head == end) (path).reverse
+    //   else if (visited.contains(queue.head)) recurse(queue.tail, visited, path.tail)
+    //   else {
+    //     val node           = queue.head
+    //     val unvisitedNodes = graph(node).filter(!visited.contains(_))
+    //     recurse(unvisitedNodes.toList ++ queue, visited + node, node :: path)
+    //   }
+    // }
+
+    // recurse(List(start), Set.empty, List.empty)
+
+    @tailrec
+    def findPathTailrec(remaining: List[(T, List[T])], visited: Set[T]): List[T] = {
+      if (remaining.isEmpty) List.empty
+      else {
+        val (node, currentPath) = remaining.head
+        if (node == end) currentPath.reverse
+        else if (visited.contains(node)) findPathTailrec(remaining.tail, visited)
+        else {
+          val neighbors = graph(node)
+          val tuples    = neighbors.map(n => (n, n :: currentPath))
+          findPathTailrec(remaining.tail ++ tuples, visited + node)
+        }
+      }
+    }
+
+    findPathTailrec(graph(start).map(n => (n, n :: List(start))).toList, Set.empty)
+  }
+
+  def findCycle[T](graph: Graph[T], node: T): List[T] = findPath(graph, node, node)
+
   println(outDegree(socialNetwork, "Alice")) // 3
   println(inDegree(socialNetwork, "David"))  // 2
 
   println(isPath(socialNetwork, "Alice", "Mary")) // true
-  println(isPath(socialNetwork, "Mary", "Alice")) // false
   println(isPath(socialNetwork, "Vinh", "Alice")) // false
+
+  println(findPath(socialNetwork, "Alice", "Mary")) // true
+  println(findPath(socialNetwork, "Mary", "Alice")) // false
+
+  println(findCycle(socialNetwork, "Alice"))
 }
